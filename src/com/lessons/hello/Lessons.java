d04141d82c7b91a6d7adeb60ec7eebbaeb7df792
Armen <armenchik.mmm@outlook.com>
2020-02-05 11:56:38 +0200

    test test test

--- src/com/lessons/hello/Lessons.java
+++ src/com/lessons/hello/Lessons.java
@@ -11,16 +11,18 @@ public class Lessons {
     float myFloatVal = 23.56f;
     double myDoubleVal = 56.23d;
     double num = a + b + c + myLongVal + myDoubleVal + myFloatVal;
-    System.out.println(num);
-    System.out.println(myDoubleVal / 3);
-    byte q = (byte) myLongVal;
-    System.out.println("byte " + q);
-    char c1 = 'A';
-    char c2 = '\u00A9';
-    System.out.println(c2);
-    System.out.println(c1);
-    System.out.println();
-    System.out.println("TEST COMMIT");
+        System.out.println(num);
+        System.out.println(myDoubleVal / 3);
+        byte q = (byte) myLongVal;
+        System.out.println("byte " + q);
+        char c1 = 'A';
+        char c2 = '\u00A9';
+        System.out.println(c2);
+        System.out.println(c1);
+        System.out.println();
+        System.out.println("TEST COMMIT");
+        System.out.println("TEST COMMIT");
+        System.out.println("TEST COMMIT");
 
-}
+    }
 }
